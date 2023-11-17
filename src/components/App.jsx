d0aa6c83238ba1a56583c84758e6d7fbe1501d4a
Artemskyba <artemgennadievich@icloud.com>
2023-11-17 18:03:58 +0200

    finish task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,38 +1,50 @@
-import { Component } from "react";
-import { FeedbaksWrapper} from "./App.styled";
-import { Section } from "./section";
-import { Statistics } from "./statistiks";
-import { FeedbackOptions } from "./feedkack-options";
+import { FeedbaksWrapper } from './App.styled';
+import { Section } from './section';
+import { Statistics } from './statistiks';
+import { FeedbackOptions } from './feedkack-options';
+import { useState } from 'react';
 
-export class App extends Component{
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0
+  const total = good + neutral + bad;
+  const positive = total ? Math.round((good / total) * 100) : 0;
+
+  const onLeaveFeedback = feedbackRate => {
+    switch (feedbackRate) {
+      case 'good':
+        setGood(prevGood => (prevGood += 1));
+        break;
+      case 'neutral':
+        setNeutral(prevNeutral => (prevNeutral += 1));
+        break;
+      case 'bad':
+        setBad(prevBad => (prevBad += 1));
+        break;
+      default:
+        break;
+    }
   };
 
-  onLeaveFeedback = (option) => {
-    this.setState(prevState => {
-      return {
-        [option]: prevState[option] + 1,
-      }
-    })
-  }
-
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = good + neutral + bad;
-    const positive = total ? Math.round((good / total) * 100) : 0;
-    return (
-      <FeedbaksWrapper>
-        <Section title={"Pleaseleave feebback"}>
-          <FeedbackOptions options={Object.keys(this.state)} onLeaveFeedback={this.onLeaveFeedback}/>
-        </Section>
-        <Section title={"Statistics"}>
-          <Statistics good={good} neutral={neutral} bad={bad} total={total} positive={positive}/>
-        </Section>
-      </FeedbaksWrapper>
-    )
-  }
+  return (
+    <FeedbaksWrapper>
+      <Section title={'Pleaseleave feebback'}>
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={onLeaveFeedback}
+        />
+      </Section>
+      <Section title={'Statistics'}>
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={total}
+          positive={positive}
+        />
+      </Section>
+    </FeedbaksWrapper>
+  );
 };
--- src/components/feedkack-options.jsx
+++ src/components/feedkack-options.jsx
@@ -1,10 +1,17 @@
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   return (
     <>
-      {options.map((option) => (
-        <button onClick={() => onLeaveFeedback (option)} key={option} type="button">{option}</button>
-      ))}
+      {options.map(option => {
+        return (
+          <button
+            onClick={() => onLeaveFeedback(option)}
+            key={option}
+            type="button"
+          >
+            {option}
+          </button>
+        );
+      })}
     </>
-  )
-}
-
+  );
+};
